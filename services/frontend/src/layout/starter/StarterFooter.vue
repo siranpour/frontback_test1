f63408e4c506c82a85a717170f7cdbb540eb64f4
Jane Hwa <janehwa@Janes-MacBook-Pro.local>
2021-10-03 16:30:46 -0700

    broken database setup

---
+++ services/frontend/src/layout/starter/StarterFooter.vue
@@ -0,0 +1,22 @@
+<template>
+    <footer class="footer has-cards">
+            <hr>
+        </div>
+    </footer>
+</template>
+
+
+<script>
+export default {
+  name: 'starter-footer',
+  data() {
+    return {
+      year: new Date().getFullYear()
+    }
+  }
+};
+</script>
+
+
+<style>
+</style>
---
+++ services/frontend/src/views/Landing.vue
@@ -0,0 +1,189 @@
+<template>
+    <div>
+
+        <div class="position-relative">
+            <!-- shape Hero -->
+            <section class="section-shaped my-0">
+                <div class="shape shape-style-1 shape-default shape-skew">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <div class="container shape-container d-flex">
+                    <div class="col px-0">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <h1 class="display-3  text-white">Our Mission Statement
+                                </h1>
+                                <p class="lead  text-white">This is our mission statement.</p>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- 1st Hero Variation -->
+        </div>
+
+        <section class="section section-lg">
+            <div class="container">
+                <div class="row row-grid align-items-center">
+                    <div class="col-md-6 order-md-2">
+                        <img src="img/theme/promo-1.png" class="img-fluid floating">
+                    </div>
+                    <div class="col-md-6 order-md-1">
+                        <div class="pr-md-5">
+                            <icon name="ni ni-settings-gear-65" class="mb-5" size="lg" type="success" shadow
+                                  rounded></icon>
+                            <h3>Awesome features</h3>
+                            <p>The kit comes with three pre-built pages to help you get started faster. You can change
+                                the text and images and you're good to go.</p>
+                            <ul class="list-unstyled mt-5">
+                                <li class="py-2">
+                                    <div class="d-flex align-items-center">
+                                        <badge type="success" circle class="mr-3" icon="ni ni-settings-gear-65"></badge>
+                                        <h6 class="mb-0">Carefully crafted components</h6>
+                                    </div>
+                                </li>
+                                <li class="py-2">
+                                    <div class="d-flex align-items-center">
+                                        <badge type="success" circle class="mr-3" icon="ni ni-html5"></badge>
+                                        <h6 class="mb-0">Amazing page examples</h6>
+                                    </div>
+                                </li>
+                                <li class="py-2">
+                                    <div class="d-flex align-items-center">
+                                        <badge type="success" circle class="mr-3" icon="ni ni-satisfied"></badge>
+                                        <h6 class="mb-0">Super friendly support team</h6>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+        <section class="section bg-secondary">
+            <div class="container">
+                <div class="row row-grid align-items-center">
+                    <div class="col-md-6">
+                        <div class="card bg-default shadow border-0">
+                            <img v-lazy="'img/theme/img-1-1200x1000.jpg'" class="card-img-top">
+                            <blockquote class="card-blockquote">
+                                <svg preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 583 95"
+                                     class="svg-bg">
+                                    <polygon points="0,52 583,95 0,95" class="fill-default"></polygon>
+                                    <polygon points="0,42 583,95 683,0 0,95" opacity=".2" class="fill-default"></polygon>
+                                </svg>
+                                <h4 class="display-3 font-weight-bold text-white">Design System</h4>
+                                <p class="lead text-italic text-white">The Arctic Ocean freezes every winter and much of
+                                    the sea-ice then thaws every summer, and that process will continue whatever
+                                    happens.</p>
+                            </blockquote>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="pl-md-5">
+                            <icon name="ni ni-settings" class="mb-5" size="lg" type="warning" shadow rounded></icon>
+
+                            <h3>Our customers</h3>
+                            <p class="lead">Don't let your uses guess by attaching tooltips and popoves to any element.
+                                Just make sure you enable them first via JavaScript.</p>
+                            <p>The kit comes with three pre-built pages to help you get started faster. You can change
+                                the text and images and you're good to go.</p>
+                            <p>The kit comes with three pre-built pages to help you get started faster. You can change
+                                the text and images and you're good to go.</p>
+                            <a href="#" class="font-weight-bold text-warning mt-5">A beautiful UI Kit for impactful
+                                websites</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+        <section class="section section-lg">
+            <div class="container">
+                <div class="row justify-content-center text-center mb-lg">
+                    <div class="col-lg-8">
+                        <h2 class="display-3">Our Team</h2>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 col-lg-3 mb-5 mb-lg-0">
+                        <div class="px-4">
+                            <img v-lazy="'img/theme/Stitch.jpg'"
+                                 class="rounded-circle img-center img-fluid shadow shadow-lg--hover"
+                                 style="width: 200px;">
+                            <div class="pt-4 text-center">
+                                <h5 class="title">
+                                    <span class="d-block mb-1">Devin Robison</span>
+                                    <small class="h6 text-muted">Web Developer</small>
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-3 mb-5 mb-lg-0">
+                        <div class="px-4">
+                            <img v-lazy="'img/theme/Olaf.jpg'"
+                                 class="rounded-circle img-center img-fluid shadow shadow-lg--hover"
+                                 style="width: 200px;">
+                            <div class="pt-4 text-center">
+                                <h5 class="title">
+                                    <span class="d-block mb-1">Megha Bhardwaj</span>
+                                    <small class="h6 text-muted">Marketing Strategist</small>
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-3 mb-5 mb-lg-0">
+                        <div class="px-4">
+                            <img v-lazy="'img/theme/team-3-800x800.jpg'"
+                                 class="rounded-circle img-center img-fluid shadow shadow-lg--hover"
+                                 style="width: 200px;">
+                            <div class="pt-4 text-center">
+                                <h5 class="title">
+                                    <span class="d-block mb-1">Michael Bollig</span>
+                                    <small class="h6 text-muted">UI/UX Designer</small>
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-3 mb-5 mb-lg-0">
+                        <div class="px-4">
+                            <img v-lazy="'img/theme/team-4-800x800.jpg'"
+                                 class="rounded-circle img-center img-fluid shadow shadow-lg--hover"
+                                 style="width: 200px;">
+                            <div class="pt-4 text-center">
+                                <h5 class="title">
+                                    <span class="d-block mb-1">Sarah Iranpour</span>
+                                    <small class="h6 text-muted">Founder and CEO</small>
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+
+
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  components: {}
+};
+</script>
